1a7aec5fafee22e91175c9d5078562ea0458388e
Mhmdzend22 <166401046+Mhmdzend22@users.noreply.github.com>
2024-09-19 06:04:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,52 +10,52 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7297484344:AAHdNUScmStTooJXMRJMs2zvQGFzKYD9E7A")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "24213496"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "460587b3c127073be75378abec90ff56")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001995978690"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002035889639"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "sewxiy")
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "6497757690"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6893079708"))
 
 #Port
 PORT = os.environ.get("PORT", "8030")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://madara:madara@cluster0.xqkrlic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001473043276"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001644866777"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002088793491"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001864051887"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "10")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "7200")) # auto delete in seconds
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>ʙᴀᴋᴋᴀᴀᴀ!! {first}\n\n ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</b>")
+START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ!! {first}\n\n ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</b>")
 try:
     ADMINS=[6376328008]
-    for x in (os.environ.get("ADMINS", "5115691197 6273945163 6103092779 5231212075").split()):
+    for x in (os.environ.get("ADMINS", "6893079708 6843816361 6003783733 6731240638 6521153981").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>ʜᴇʟʟᴏ {first}\nᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ʀᴇʟᴏᴀᴅ button ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>ʜᴇʟʟᴏ {first}\nᴋᴀʟɪᴀɴ ᴡᴀᴊɪʙ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ᴀɢᴀʀ ᴅᴀᴘᴀᴛ ᴍᴇɴᴏɴᴛᴏɴ ᴠɪᴅᴇᴏ !! \nᴘɪʟɪʜ "ꜱᴛᴀʀᴛ" ᴘᴀᴅᴀ ʙᴏᴛ \nᴍᴀᴋᴀ ᴀᴋᴀɴ ᴍᴜɴᴄᴜʟ 2 ᴛᴏᴍʙᴏʟ "ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ" \nᴘɪʟɪʜ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ꜱᴀᴛᴜ ᴘᴇʀꜱᴀᴛᴜ ᴍᴀᴋᴀ ᴋᴀʟɪᴀɴ ᴀᴋᴀɴ ᴅɪ\nᴀʀᴀʜᴋᴀɴ ᴍᴀꜱᴜᴋ ᴋᴇ ᴄʜᴀɴɴᴇʟ ᴡᴀᴊɪʙ ᴘɪʟɪʜ ꜱᴜʙꜱᴄʀɪʙᴇ/ʙᴇʀʟᴀɴɢɢᴀɴᴀɴ \nᴋᴇᴍʙᴀʟɪ ᴋᴇ ʙᴏᴛ \nᴘɪʟɪʜ "ʀᴇʟᴏᴀᴅ/ᴍᴜʟᴀɪ ᴜʟᴀɴɢ" \nꜱᴇʟᴀᴍᴀᴛ ɴɢᴏᴄᴏᴋ ... \n\nᴊᴏɪɴ  ɢʀᴜʙ ᴠɪᴘ ᴘᴇʀᴍᴀɴᴇɴ @tyaa86")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @OtakuFlix_Network</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ᴊᴏɪɴ ɢʀᴜᴘ ᴠɪᴘ ᴘᴇʀᴍᴀɴᴇɴ.\nᴀᴅᴍɪɴ 1 : @tyaa86 \nᴀᴅᴍɪɴ 2 : @minjoinvip \n\nʟɪꜱᴛ ᴠɪᴘ : https://t.me.rajakonten_testi/83")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
@@ -64,7 +64,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
+USER_REPLY_TEXT = "ᴊᴏɪɴ ɢʀᴜᴘ ᴠɪᴘ ᴘᴇʀᴍᴀɴᴇɴ.\nᴀᴅᴍɪɴ 1 : @tyaa86 \nᴀᴅᴍɪɴ 2 : @minjoinvip \n\nʟɪꜱᴛ ᴠɪᴘ : https://t.me.rajakonten_testi/83"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6497757690)
